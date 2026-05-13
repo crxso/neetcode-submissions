ebd4da30df025c7d9fd655375722469ec2c15834
crxso <46763991+crxso@users.noreply.github.com>
2026-05-12 22:10:13 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 5
+print(type(variable))
+
+variable = 5.5
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "false"
+print(type(variable))
+
+variable = [1, 2, 3]
+print(type(variable))
